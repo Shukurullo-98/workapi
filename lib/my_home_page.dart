69ca914ab5ec34d49d3fe3518005236ee7e85b7a
Shukurullo <tbekamov@gmail.com>
2023-05-02 23:57:12 +0500

    .

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:http/http.dart' as https;
 import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:work_api/top_level_data.dart';
 import 'package:work_api/user_data.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -13,66 +16,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Future<UserData> getUserData() async {
     try {
       Response response =
-          await https.get(Uri.parse("https://api.agify.io/?name=bella"));
-      UserData userData = UserData.fromJson(jsonDecode(response.body));
-      return userData;
+      await https.get(Uri.parse("https://api.agify.io/?name=bella"));
+      if (response.statusCode == 200) {
+        UserData userData = UserData.fromJson(jsonDecode(response.body));
+        return userData;
+      } else {
+        throw Exception();
+      }
     } on Exception catch (e) {
       throw Exception(e);
     }
   }
 
+  Future<TopLevelData> getMemes() async {
+    try {
+      Response response =
+      await https.get(Uri.parse("https://api.imgflip.com/get_memes"));
+      if (response.statusCode == 200) {
+        TopLevelData topLevelData =
+        TopLevelData.fromJson(jsonDecode(response.body));
+        return topLevelData;
+      } else {
+        throw Exception();
+      }
+    } on Exception catch (e) {
+      throw Exception(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Working Api"),
+        title: Text("linkify"),
       ),
-      body: FutureBuilder<UserData>(
-        future: getUserData(),
-        builder: (BuildContext context, AsyncSnapshot<UserData> snapshot) {
-          if (snapshot.hasData) {
-            UserData? userData = snapshot.data;
-            return Container(
-              child: Center(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('name: '),
-                        Text(userData!.name),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Age: '),
-                        Text(userData.age.toString()),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Count: '),
-                        Text(userData.count.toString()),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else {
-            return Container(
-              child: Center(child: CircularProgressIndicator()),
-            );
-          }
-        },
+      body: Container(
+        child: Center(
+
+          child: Linkify(text: "https://kun.uz",
+            options: LinkifyOptions(humanize: false),
+            onOpen: (link) async{
+            await launchUrl(Uri.parse(link.url));
+            print(link);
+            },
+          ),
+        ),
       ),
     );
   }
 }
+
+// Scaffold(
+// appBar: AppBar(
+// title: Text("Working Api"),
+// ),
+// body: FutureBuilder<TopLevelData>(
+// future: getMemes(),
+// builder: (BuildContext context, AsyncSnapshot<TopLevelData> snapshot) {
+// if (snapshot.hasData) {
+// // TopLevelData? topLevelData = snapshot.data;
+// return Container(
+// child: Center(
+// child: Column(
+// children: [
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+// Text('name: '),
+// Text(snapshot.data!.data.memes[0].name),
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+// Text('Age: '),
+// // Text(userData.age.toString()),
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// children: [
+// Text('Count: '),
+// // Text(userData.count.toString()),
+// ],
+// ),
+// ],
+// ),
+// ),
+// );
+// } else {
+// return Container(
+// child: Center(child: CircularProgressIndicator()),
+// );
+// }
+// },
+// ),
+// );
--- lib/top_level_data.dart
+++ lib/top_level_data.dart
@@ -1,14 +1,17 @@
 import 'package:work_api/data_list.dart';
+import 'package:work_api/mems.dart';
 
 class TopLevelData {
   TopLevelData({required this.data, required this.status});
 
-  final String status;
+  final bool status;
   final DataList data;
 
-  factory TopLevelData.fromJson(Map<String, Object?> json) {
+  factory TopLevelData.fromJson(Map<String, dynamic> json) {
+    var jsonData = json["data"]["memes"] as List;
+    List<Memes> memes = jsonData.map((mem)=> Memes.fromJson(mem)).toList();
     return TopLevelData(
-        data: DataList.fromJson(json['memes'].toString()),
-        status: json['success'] as String? ?? "");
+        data: DataList(memes: memes),
+        status: json['success'] as bool? ?? false);
   }
 }
