c89ed9815646571332e1c28836a1c1b7311fed5d
Shukurullo <tbekamov@gmail.com>
2023-05-03 00:24:32 +0500

    .

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -19,7 +19,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<UserData> getUserData() async {
     try {
       Response response =
-      await https.get(Uri.parse("https://api.agify.io/?name=bella"));
+          await https.get(Uri.parse("https://api.agify.io/?name=bella"));
       if (response.statusCode == 200) {
         UserData userData = UserData.fromJson(jsonDecode(response.body));
         return userData;
@@ -34,10 +34,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<TopLevelData> getMemes() async {
     try {
       Response response =
-      await https.get(Uri.parse("https://api.imgflip.com/get_memes"));
+          await https.get(Uri.parse("https://api.imgflip.com/get_memes"));
       if (response.statusCode == 200) {
         TopLevelData topLevelData =
-        TopLevelData.fromJson(jsonDecode(response.body));
+            TopLevelData.fromJson(jsonDecode(response.body));
         return topLevelData;
       } else {
         throw Exception();
@@ -49,21 +49,86 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return
+        // return Scaffold(
+        //   appBar: AppBar(
+        //     title: Text("linkify"),
+        //   ),
+        //   body: Container(
+        //     child: Center(
+        //
+        //       child: Linkify(text: "https://kun.uz",
+        //         options: LinkifyOptions(humanize: false),
+        //         onOpen: (link) async{
+        //         await launchUrl(Uri.parse(link.url));
+        //         print(link);
+        //         },
+        //       ),
+        //     ),
+        //   ),
+        // );
+        Scaffold(
       appBar: AppBar(
-        title: Text("linkify"),
+        title: Text("Working Api"),
       ),
-      body: Container(
-        child: Center(
-
-          child: Linkify(text: "https://kun.uz",
-            options: LinkifyOptions(humanize: false),
-            onOpen: (link) async{
-            await launchUrl(Uri.parse(link.url));
-            print(link);
-            },
-          ),
-        ),
+      body: FutureBuilder<TopLevelData>(
+        future: getMemes(),
+        builder: (BuildContext context, AsyncSnapshot<TopLevelData> snapshot) {
+          if (snapshot.hasData) {
+            var mems = snapshot.data!.data.memes;
+            return ListView(
+              children: List.generate(
+                mems.length,
+                (index) {
+                  var item = mems[index];
+                  return Container(
+                    margin: EdgeInsets.all(16),
+                    padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 6,
+                            blurRadius: 5,
+                            offset: Offset(1, 3),
+                            color: Colors.grey.shade300,
+                          )
+                        ]),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 400,
+                          child: Image.network(item.url),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("ID" ),
+                            Text(item.id, )
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Name" ),
+                            Text(item.name, )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          } else {
+            return Container(
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+        },
       ),
     );
   }
